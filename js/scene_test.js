333b8fb952f4d693ee9115479ae5cf81c136df46
Sirej Dua <sirejdua@gmail.com>
2019-04-28 15:54:36 -0700

    trying out a mesh

--- js/scene_test.js
+++ js/scene_test.js
@@ -5,17 +5,25 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var loader = new THREE.GLTFLoader();
+loader.load( '../../models/teacup/scene.gltf', function (gltf) {
+    scene.add( gltf.scene );
+}, undefined, function (error) {
+    console.error( error );
+});
+/*
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshBasicMaterial({color: 0x76b900});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+*/
 
 camera.position.z = 5;
-
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
+    /*cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    */
     renderer.render(scene, camera);
 }
 
